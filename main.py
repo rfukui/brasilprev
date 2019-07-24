456f0d9642a61ae66da5df7ccb37424cea3d4b7e
rfukui <ricardo.fukui@resultadosdigitais.com.br>
2019-07-24 06:36:58 -0300

    linter e readme ajustado

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ min_value_property = 50
 max_value_property = 500
 percent_rent = 0.25
 increase_balance_by_checkpoint = 100
-    
+
 args = {
     0: "300",
     1: "1000",
@@ -23,66 +23,84 @@ args = {
 }
 
 players = [
-    {"id":0,
-    "type":"impulsive",
-    "balance": start_balance,
-    "position": 0},
-    {"id":1,
-    "type":"picky",
-    "balance": start_balance,
-    "position": 0},
-    {"id":2,
-    "type":"prudent",
-    "balance": start_balance,
-    "position": 0},
-    {"id":3,
-    "type": "rand",
-    "balance": start_balance,
-    "position": 0}
+    {
+        "id": 0,
+        "type": "impulsive",
+        "balance": start_balance,
+        "position": 0
+    },
+    {
+        "id": 1,
+        "type": "picky",
+        "balance": start_balance,
+        "position": 0
+    },
+    {
+        "id": 2,
+        "type": "prudent",
+        "balance": start_balance,
+        "position": 0
+    },
+    {
+        "id": 3,
+        "type": "rand",
+        "balance": start_balance,
+        "position": 0
+    }
     ]
 
 properties = []
 total_plays = 0
 resume = {
     "timeouts": 0,
-    "avg_plays":0,
+    "avg_plays": 0,
     "impulsive": 0,
     "picky": 0,
     "prudent": 0,
     "rand": 0,
     }
+
+
 def create_properties():
-    
+
     for i in range(qtd_properties):
-        buy_value = random.randint(min_value_property,max_value_property)
+        buy_value = random.randint(min_value_property, max_value_property)
         properties.append({
-            "id":i,
+            "id": i,
             "buy_value": buy_value,
-            "rent_value":int(buy_value * percent_rent),
-            "owner":None,
+            "rent_value": int(buy_value * percent_rent),
+            "owner": None,
             }
         )
+
 
 def reset_total_plays():
     global total_plays
     total_plays = 0
 
+
 def reset_players():
-    for p in players:
-        players[p["id"]]["balance"] = start_balance
-        players[p["id"]]["position"] = 0
-        
+    random.shuffle(players)
+    for i, p in enumerate(players, start=0):
+        players[i]["id"] = i
+        players[i]["balance"] = start_balance
+        players[i]["position"] = 0
+
+
 def reset_porperties():
 
     for prop in properties:
         properties[prop["id"]]["owner"] = None
 
+
 def die():
 
-    return random.randint(1,6)
+    return random.randint(1, 6)
 
-def increase_balance(player, value = increase_balance_by_checkpoint):
-    players[player["id"]]["balance"] += value 
+
+def increase_balance(player, value=increase_balance_by_checkpoint):
+    players[player["id"]]["balance"] += value
+
 
 def position(player):
     actual_position = (player["position"] + die()) % qtd_properties
@@ -90,10 +108,12 @@ def position(player):
         increase_balance(player)
     players[player["id"]]["position"] = actual_position
 
+
 def set_property_owner(player, prop):
     if player["id"] is not None:
         increase_balance(player, -prop["buy_value"])
     properties[prop["id"]]["owner"] = player["id"]
+
 
 def buy_by_type(player, prop):
     if player["type"] == "impulsive":
@@ -102,73 +122,85 @@ def buy_by_type(player, prop):
         set_property_owner(player, prop)
     elif player["type"] == "prudent" and (player["balance"] - prop["buy_value"]) > 80:
         set_property_owner(player, prop)
-    elif player["type"] == "rand" and random.randint(0,1):
+    elif player["type"] == "rand" and random.randint(0, 1):
         set_property_owner(player, prop)
+
 
 def remove_properties(player):
     for p in properties:
         if player["id"] == p["owner"]:
             set_property_owner({"id": None}, p)
-def pay_rent(player,prop):
+
+
+def pay_rent(player, prop):
     if player["balance"] >= prop["rent_value"]:
         increase_balance(player, -prop["rent_value"])
     else:
         increase_balance(players[prop["owner"]], player["balance"])
         remove_properties(player)
-        players[player["id"]]["balance"] =- 1
+        players[player["id"]]["balance"] = -1
 
-def check_to_buy(player,prop):
+
+def check_to_buy(player, prop):
     if player["balance"] > prop["buy_value"]:
         buy_by_type(player, prop)
+
 
 def buy_or_rent(player):
     prop = properties[player["position"]]
     if prop["owner"] is None:
         check_to_buy(player, prop)
     else:
-        pay_rent(player, prop)           
+        pay_rent(player, prop)
+
 
 def play(player):
     if player["balance"] != -1:
         position(player)
         buy_or_rent(player)
-    
+
+
 def check_players():
     i = 0
     for player in players:
         if player["balance"] == -1:
-            i +=1
-    
+            i += 1
+
     return i >= 3
+
 
 def game():
     global total_plays
     for i in range(1, (qtd_plays + 1)):
         for player in players:
             play(player)
-    
+
         if check_players():
             total_plays = i
             return
     resume["timeouts"] += 1
     total_plays = qtd_plays
 
+
 def increment_player_resume(player):
     resume[player["type"]] += 1
 
+
 def populate_resume(i):
-    increment_player_resume(sorted(players, key = lambda p: p["balance"], reverse=True)[0])
-    resume["avg_plays"] = resume["avg_plays"] + (total_plays - resume["avg_plays"]) / i 
+    increment_player_resume(sorted(players, key=lambda p: p["balance"], reverse=True)[0])
+    resume["avg_plays"] = resume["avg_plays"] + (total_plays - resume["avg_plays"]) / i
+
 
 def reset():
     reset_porperties()
     reset_players()
     reset_total_plays()
 
+
 def main():
     create_properties()
     all_games = []
-    for i in range(1,(qtd_simulation + 1)):
+    for i in range(1, (qtd_simulation + 1)):
         reset()
         game()
         populate_resume(i)
